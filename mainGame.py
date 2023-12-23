10c9abce8f40f6c435c9ccc129e893268d84ac01
Santiago <santiago14.secundaria@gmail.com>
2023-12-23 16:52:43 -0500

    Health bar and basic attacks working, however there are still some serious bugs in the movement related to atacking trigger

--- mainGame.py
+++ mainGame.py
@@ -10,6 +10,17 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawler")
 
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#Define colors
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+
+
 #load background image
 bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
 
@@ -17,6 +28,14 @@ bg_image = pygame.image.load("assets/images/background/background.jpg").convert_
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDHT, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
+    
+#function for drawing fighter health bars
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x-5, y-5, 410, 40))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+    
     
 #create two instances of fighters
 fighter_1 = Fighter(200, 310)
@@ -26,8 +45,16 @@ fighter_2 = Fighter(700, 310)
 run = True
 while run:
     
+    clock.tick(FPS)
+    
     #draw background
     draw_bg()
+    
+    #show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+    #move fighters
+    fighter_1.move(SCREEN_WIDHT, SCREEN_HEIGHT, screen, fighter_2)
     
     #draw fighters
     fighter_1.draw(screen)
