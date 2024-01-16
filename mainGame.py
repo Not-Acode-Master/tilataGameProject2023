3a3316ea3996a22bf157a76abfe788317d21ec23
Santiago <santiago14.secundaria@gmail.com>
2024-01-15 21:24:45 -0500

    Basic pause menu with good logic :)

--- mainGame.py
+++ mainGame.py
@@ -2,6 +2,7 @@ import pygame
 from pygame import mixer
 from fighter import Fighter
 from menu import pauseMenu
+from button import Button
 
 mixer.init()
 pygame.init()
@@ -63,6 +64,10 @@ wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").conv
 #load victory image
 victory_img = pygame.image.load("assets/images/icons/victory.png").convert_alpha()
 
+#load buttons with its instances
+play_button_img = pygame.image.load("assets/images/buttons/play.png").convert_alpha()
+quit_button_img = pygame.image.load("assets/images/buttons/quit.png").convert_alpha()
+
 #define number of steps in each animation
 WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
 WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
@@ -93,8 +98,12 @@ def draw_health_bar(health, x, y):
 fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
 fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
 
+#create button instances
+play_button = Button(300, 120, play_button_img, 4)
+quit_button = Button(300, 210, quit_button_img, 4)
+
 #menu instanes
-menu1 = pauseMenu(False)
+menu1 = pauseMenu(False, RED, screen, play_button, quit_button)
 
 #game loop
 run = True
@@ -102,65 +111,70 @@ while run:
     
     clock.tick(FPS)
     
-    #update menu state
-    menu1.aditionalControls()
-    menu1.update()
-    
-    #draw background
-    draw_bg()
-    
-    #show player stats
-    draw_health_bar(fighter_1.health, 20, 20)
-    draw_health_bar(fighter_2.health, 580, 20)
-    draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
-    draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
-    
-    #update countdown 
-    if intro_count <= 0:
-        #move figher
-        if menu1.paused == False:
-            fighter_1.move(SCREEN_WIDHT, SCREEN_HEIGHT, screen, fighter_2, round_over, sword_fx)
-            fighter_2.move(SCREEN_WIDHT, SCREEN_HEIGHT, screen, fighter_1, round_over, magic_fx)
-    else:
-        #display count timera
-        draw_text(str(intro_count), count_font, RED, SCREEN_WIDHT / 2, SCREEN_HEIGHT / 3)
-        #update count timer
-        if (pygame.time.get_ticks() - last_count_update ) >= 1000:
-            intro_count -= 1
-            last_count_update = pygame.time.get_ticks()
-        
-    #update fighters
-    fighter_1.update()
-    fighter_2.update()
-    
-    
-    #draw fighters
-    fighter_1.draw(screen)
-    fighter_2.draw(screen)
-    
-    #check for player defeat
-    if round_over == False:
-        if fighter_1.alive == False:
-            score[1] += 1
-            round_over = True
-            round_over_time = pygame.time.get_ticks()
-        elif fighter_2.alive == False:
-            score[0] += 1
-            round_over = True
-            round_over_time = pygame.time.get_ticks()
-    else:
-         #display victory image
-         screen.blit(victory_img, (360, 150))
-         if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
-            round_over = False
-            intro_count = 4
-            fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
-            fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
-    
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_p:
+                menu1.paused = not menu1.paused
+                
+            
+    #update menu state
+    menu1.update(run)
+    run = menu1.runt
+    
+    #check if the game is paused
+    if menu1.paused == False:
+        #draw background
+        draw_bg()
+        
+        #show player stats
+        draw_health_bar(fighter_1.health, 20, 20)
+        draw_health_bar(fighter_2.health, 580, 20)
+        draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
+        draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
+        
+        #update countdown 
+        if intro_count <= 0:
+                fighter_1.move(SCREEN_WIDHT, SCREEN_HEIGHT, screen, fighter_2, round_over, sword_fx)
+                fighter_2.move(SCREEN_WIDHT, SCREEN_HEIGHT, screen, fighter_1, round_over, magic_fx)
+        else:
+            #display count timera
+            draw_text(str(intro_count), count_font, RED, SCREEN_WIDHT / 2, SCREEN_HEIGHT / 3)
+            #update count timer
+            if (pygame.time.get_ticks() - last_count_update ) >= 1000:
+                intro_count -= 1
+                last_count_update = pygame.time.get_ticks()
+            
+        #update fighters
+        fighter_1.update()
+        fighter_2.update()
+        
+        
+        #draw fighters
+        fighter_1.draw(screen)
+        fighter_2.draw(screen)
+        
+        #check for player defeat
+        if round_over == False:
+            if fighter_1.alive == False:
+                score[1] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+            elif fighter_2.alive == False:
+                score[0] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+        else:
+            #display victory image
+            screen.blit(victory_img, (360, 150))
+            if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+                round_over = False
+                intro_count = 4
+                fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+                fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
+    
     
     #update display
     pygame.display.update()
--- menu.py
+++ menu.py
@@ -1,17 +1,18 @@
 import pygame
 
 class pauseMenu():
-    def __init__(self, paused):
+    def __init__(self, paused, screen_color, surface, play_button, quit_button):
         self.paused = paused
-    
-    def aditionalControls(self):
-        key = pygame.key.get_pressed()
+        self.screen_color = screen_color
+        self.surface = surface
+        self.play_button = play_button
+        self.quit_button = quit_button
         
-        if key [pygame.K_p]:
-            self.paused = not self.paused
-        
-    def update(self):
+    def update(self, runt):
+        self.runt = runt
         if self.paused == True:
-            print("Paused")
-        else:
-            print("Playing")
+            self.surface.fill(self.screen_color)
+            if self.play_button.draw(self.surface):
+                self.paused = not self.paused
+            elif self.quit_button.draw(self.surface):
+                self.runt = False
